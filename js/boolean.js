44ed87581e561e1436402a815420b79a46170081
Griselda <zeldagris82@gmail.com>
2019-04-23 20:08:38 -0700

    learned boolean and NaN

---
+++ js/boolean.js
@@ -0,0 +1,13 @@
+//boolean data type can only be true or false. no quotations
+var a = true; // true equals to 1
+var b = false; // false equals to 0
+
+console.log(12 == '12');
+
+
+//isNaN returns only boolean output : true/false
+console.log(isNaN(2000));
+console.log(isNaN('2'));
+console.log(isNaN(a));
+console.log(isNaN('a'));
+console.log(isNaN(true));
