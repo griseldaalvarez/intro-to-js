0ab9d754ca02b4af659780f5edb783471881d4a3
Griselda <zeldagris82@gmail.com>
2019-04-23 20:57:11 -0700

    objects

---
+++ js/object.js
@@ -0,0 +1,21 @@
+//object data type. defined by curly braquets
+//every object has a key and one value
+
+var obj = {}; //empty object
+var car = {
+    make: 'honda', //key=make  //value=honda
+    model: 'civic',
+    year: 2019
+};
+console.log(car); //gives us everything; keys and values
+console.log(car.model); //gives us only the model type
+console.log(car.year); //gives us only the year
+
+var date = {
+    date: 23,
+    month: 'April',
+    year: 2019,
+    day: 'Tuesday'
+};
+console.log(date.day);
+
