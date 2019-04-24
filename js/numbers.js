01f0951193ad06450ccbbdfb88edca552fd9509f
Griselda <zeldagris82@gmail.com>
2019-04-23 19:04:56 -0700

    changed the scripts.js to numbers.js and created a new string.js file

---
+++ js/numbers.js
@@ -0,0 +1,40 @@
+// number data types
+
+var a = 10;
+console.log(a);
+
+var b = 100;
+console.log(b);
+
+var a = 20;
+console.log(a);
+
+var c = 0x1E;  //hex value of 30
+console.log(c);
+
+var d = 0.5;
+console.log(d);
+
+
+// addition
+var sum = a + b;
+console.log(sum);
+
+// subtraction
+var diff = d - c;
+console.log(diff);
+
+// multiplication
+var times = a * d;
+console.log(times);
+
+// division
+var div = a / d;
+console.log(div);
+
+// modulus  this gives the remainder
+var mod = b % a;
+console.log(mod);
+
+var e = '5';  //not a number; rather a 'string'
+console.log(typeof e); //returns in console the type of variable e (ie=string),not the num.5
